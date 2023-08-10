ebe8275b4482d815346a3e1dbed79dc406927699
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-08-11 01:40:52 +0700

    add_employer

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\DisplayConfig;
-use App\Models\Employer;
-use App\Models\Teacher;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,16 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(100)->create();
+        \App\Models\User::factory(100)->create();
         $this->call(UserSeeder::class);
         $this->call(MajorSeeder::class);
         $this->call(SpecialtySeeder::class);
-        // $this->call(StudentSeeder::class);
+        $this->call(StudentSeeder::class);
         $this->call(EmployerSeeder::class);
         $this->call(TeacherSeeder::class);
         $this->call(DisplayConfigSeeder::class);
-        // $this->call(RegisterSpecialtySeeder::class);
-        // $this->call(RegisterSpecialtyDetailSeeder::class);
+        $this->call(RegisterSpecialtySeeder::class);
+        $this->call(RegisterSpecialtyDetailSeeder::class);
         $this->call(RecruitmentPositionSeeder::class);
         $this->call(InterCompanySeeder::class);
         $this->call(InternRegistrationSeeder::class);
