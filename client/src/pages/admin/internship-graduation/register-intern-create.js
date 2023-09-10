8af7bd8c97f18fdbab1f711d4bb6ff2ac9011e31
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2023-09-11 00:55:57 +0700

    Update database

--- client/src/pages/admin/internship-graduation/register-intern-create.js
+++ client/src/pages/admin/internship-graduation/register-intern-create.js
@@ -4,8 +4,11 @@ import MainCard from 'components/MainCard';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import CreateReigisterInternForm from 'sections/admin/internship-graduation/register_intern_create/CreateReigisterInternForm';
+import { useParams } from 'react-router';
 
 const RegisterIntern = () => {
+  const { Id } = useParams();
+  console.log(Id);
   return (
     <>
       <Typography variant="h4" component="h1" mb={2}>
