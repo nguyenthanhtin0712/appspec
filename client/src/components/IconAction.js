049d5ebaad8d27fc0157b29dae281774f7699f79
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2023-09-10 23:01:20 +0700

    refactor code

--- client/src/components/IconAction.js
+++ client/src/components/IconAction.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import { Link } from 'react-router-dom';
 
-const IconAction = () => {
-  return <div></div>;
+const IconAction = ({ title, href, onClick, icon, ...props }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton component={href ? Link : undefined} to={href ?? undefined} onClick={onClick ?? undefined} {...props}>
+        {icon}
+      </IconButton>
+    </Tooltip>
+  );
 };
 
 export default IconAction;
