109a66306cd35940e9ee3b0f37a0784769ab6d00
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-09-19 14:23:13 +0700

    Update JobholderSearchItem.js

--- client/src/sections/admin/assignment_intern/JobholderSearchItem.js
+++ client/src/sections/admin/assignment_intern/JobholderSearchItem.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { ListItemButton, ListItemText } from '@mui/material';
 import { dispatch } from 'store/index';
-import { joinInternship, getJobholderIntenship } from 'store/reducers/assignmentIntenship';
+import { joinInternship, getJobholderIntenship, fetchData } from 'store/reducers/assignmentIntenship';
+import { useSelector } from 'react-redux';
 
 const JobholderSearchItem = ({ jobholder_code, id, name, join }) => {
   const [state, setState] = useState(join);
+  const { columnFilters, globalFilter, sorting, pagination, status, assignment_intern_id } = useSelector(
+    (state) => state.assignment_internship
+  );
   const handleClick = () => {
     const change = async () => {
       await dispatch(joinInternship({ id, jobholder_code }));
       await dispatch(getJobholderIntenship(id));
+      await dispatch(fetchData({ columnFilters, globalFilter, sorting, pagination, status, assignment_intern_id }));
     };
     change();
     setState(state === 0 ? 1 : 0);
