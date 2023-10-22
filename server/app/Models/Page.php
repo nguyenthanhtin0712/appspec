83a08c483c7eb1169ad0ef0d0910c6da6c31139f
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2023-10-23 01:42:13 +0700

    Add API page

---
+++ server/app/Models/Page.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
+
+class Page extends Model
+{
+    use HasFactory, HasRoles;
+    protected $table = 'pages';
+    protected $primaryKey = 'page_id';
+    protected $fillable = [
+        'page_id',
+        'page_slug',
+        'page_title',
+        'page_content',
+        'page_isDelete'
+    ];
+}
