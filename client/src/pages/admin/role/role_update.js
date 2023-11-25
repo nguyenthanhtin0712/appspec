8f70195f9e2a4327ffc48aaf499254d4b6348fd4
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-11-25 23:45:48 +0700

    fix role update not show

--- client/src/pages/admin/role/role_update.js
+++ client/src/pages/admin/role/role_update.js
@@ -8,7 +8,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import * as Yup from 'yup';
 import { dispatch } from 'store/index';
-import { getFunctional, setSelectedCheckboxes, updateRole } from 'store/slices/roleSlice';
+import { getFunctional, setSelectedCheckboxes, updateRole, getRole } from 'store/slices/roleSlice';
 import RoleCard from 'sections/admin/role/RoleCard';
 import LoadingBox from 'components/LoadingBox';
 import { Backdrop, CircularProgress, Stack } from '@mui/material';
@@ -21,11 +21,10 @@ const RoleUpdate = () => {
   const [isLoadingData, setIsLoadingData] = useState(true);
   const { functional, isLoadingFunc, selectedCheckboxes, isLoadingCreate, infoRole } = useSelector((state) => state.role);
 
-  console.log(functional);
-
   useEffect(() => {
     const getFunc = async () => {
       await dispatch(getFunctional());
+      await dispatch(getRole(id));
       setIsLoadingData(false);
     };
     getFunc();
